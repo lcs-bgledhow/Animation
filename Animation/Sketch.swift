3b59661b2705e7db1d86738d0ff5322323795a57
Ben Gledhow <bgledhow@lcs-mba3210.lcs.on.ca>
2017-10-28 11:45:06 -0400

    Finished Four corners even tho it says Bouncing Ball it is the four corners

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,16 +8,19 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var offset : Int
     // This function runs once
     override init() {
+        
+        
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
         x = 250
-        
+        offset = 250
+ 
     }
     
     // Runs in a loop, forever, to create the animated effect
@@ -25,9 +28,18 @@ class Sketch : NSObject {
         
         // Change position
         x += 1
-        
+        offset += 1
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawShapesWithBorders = false
+        
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: offset, centreY: offset, width: 50, height: 50)
+        canvas.fillColor = Color.yellow
+        canvas.drawEllipse(centreX: offset, centreY: 500 - offset, width: 50, height: 50)
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: 500 - offset, centreY: offset, width: 50, height: 50)
+        canvas.fillColor = Color.red
+        canvas.drawEllipse(centreX: 500 - offset, centreY: 500 - offset, width: 50, height: 50)
         
     }
     
