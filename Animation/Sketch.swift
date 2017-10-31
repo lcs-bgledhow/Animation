f15bd21327a949316c3c116d8c4bdb71370ac390
Ben Gledhow <bgledhow@lcs-mba3210.lcs.on.ca>
2017-10-31 15:13:25 -0400

    Finished Cannon Ball

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,10 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
+    var dx: Int
+    var dy: Int
+    var a: Double
     
     // This function runs once
     override init() {
@@ -16,18 +20,25 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
+        y = 0
+        dy = 1
+        dx = 1
+        a = 500/(250 * -250)
+        
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
         
+        x += dx
+        y = Int(a * Double(x) * (Double(x)-500))
+        
+
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
         
     }
     
